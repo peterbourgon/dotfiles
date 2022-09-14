32755f6e5b32c612a83d0cac2de37408fe3f4a23
Peter Bourgon <peter@bourgon.org>
2022-09-14 20:38:16 +0200

    Updates

--- fish_functions/coln.fish
+++ fish_functions/coln.fish
@@ -1,5 +1,5 @@
-function coln --description "Take whitespace-delimited columns from each line of input"
-	set prefixed (string replace --regex '^(.*)$' '\$$$1' $argv)
-	set pattern  (string join ', ' $prefixed)
-	awk '{print '$pattern'}'
+function coln --description 'Take whitespace-delimited columns from each line of input'
+	set a (for c in $argv ; echo "\$$c" ; end)
+	set b (string join ', ' $a)
+	awk '{print '$b'}' | column -t
 end
